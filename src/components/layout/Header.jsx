589c6815ce31efded213a816c9887ae45580b577
anjiyun <iirisii7465@gmail.com>
2026-08-24 18:47:02 +0900

    .

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,39 +1,57 @@
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../../features/auth/authSlice';
-import './Header.css';
+import { Link } from 'react-router-dom'; 
+import { useSelector, useDispatch } from 'react-redux'; 
+import { logout } from '../../features/auth/authSlice'; 
+import logo from '../../assets/images/yomi_logo_jp.png'; 
+import './Header.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'; 
+ 
+function Header() { 
+  const dispatch = useDispatch(); 
+  const accessToken = useSelector((state) => state.auth.accessToken); 
+ 
+  const handleLogout = () => { 
+    dispatch(logout()); 
+  }; 
+ 
+  return ( 
+    <header className="header"> 
 
-function Header() {
-  const dispatch = useDispatch();
-  const accessToken = useSelector((state) => state.auth.accessToken);
+      <div className='logo'> 
+        <Link to="/">
+          <img src={logo} alt="#" />
+        </Link> 
+      </div> 
+ 
+      <div className='featurs'> 
+        <Link to='featurs/' className='featurs_menu'>海外購入</Link> 
+        <Link to='featurs/' className='featurs_menu'>共同購入</Link> 
+        <Link to='featurs/' className='featurs_menu'>ご利用案内</Link> 
+        <Link to='featurs/' className='featurs_menu'>カスタマーセンター</Link> 
+      </div> 
+ 
+      <div className='member'> 
+ 
+     <Link to="login/" className='login_bt'>
+         번역은어떻게넣는거지..?
+        </Link> 
+    
+        <button className='search_bt'> 
+          <FontAwesomeIcon icon={faMagnifyingGlass} /> 
+        </button> 
+ 
+        <Link to="login/" className='login_bt'>
+          ログイン
+        </Link> 
 
-  const handleLogout = () => {
-    dispatch(logout());
-  };
-
-  return (
-    <header className="header">
-      <div className="header-inner">
-        <Link to="/" className="header-logo">
-          YoRiMiChi
-        </Link>
-
-        <nav className="header-nav">
-          {accessToken ? (
-            <>
-              <Link to="/mypage">마이페이지</Link>
-              <button onClick={handleLogout}>로그아웃</button>
-            </>
-          ) : (
-            <>
-              <Link to="/login">로그인</Link>
-              <Link to="/signup">회원가입</Link>
-            </>
-          )}
-        </nav>
-      </div>
-    </header>
-  );
-}
-
+        <Link to="join/" className='join_bt'>
+          会員登録
+        </Link> 
+ 
+      </div> 
+ 
+    </header> 
+  ); 
+} 
+ 
 export default Header;
