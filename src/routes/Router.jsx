f5833f3b2302450e2a5dbed35940a15e59b403c9
anjiyun <iirisii7465@gmail.com>
2026-08-27 12:08:14 +0900

    add: new code page collaboration shopping

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,13 +3,14 @@ import Layout from '../components/layout/Layout';
 import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
 import PrivateRoute from './PrivateRoute';
-
+import GroupPurchase from "../pages/group_purchase/group_purchase";
 function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+      <Route path="/groupbuy" element={<GroupPurchase />} />
 
         <Route element={<PrivateRoute />}> {/* 로그인 이후 사용 가능 router */}
           {/* <Route path="/mypage" element={<MyPage />} /> */}
