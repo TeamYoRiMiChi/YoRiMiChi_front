bda90b469043f4abab1d389a0f3ee6da5c8ea035
An-Toku <anduk1815@gmail.com>
2026-09-01 12:36:31 +0900

    라우터 경로명 join에서 signup으로 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,17 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from '../components/layout/Layout';
-import Home from '../pages/home/Home';
-import Login from '../pages/login/Login';
-import PrivateRoute from './PrivateRoute';
-import Faq from '../pages/Faq/Faq';
-
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import Home from "../pages/home/Home";
+import Login from "../pages/Login/Login";
+import PrivateRoute from "./PrivateRoute";
+import Faq from "../pages/Faq/Faq";
 
 import GroupPurchase from "../pages/group_purchase/group_purchase";
-import Overseas from '../pages/Overseas/Overseas';
-import Guide from '../pages/Guide/Guide';
-import CustomerService from '../pages/Customer_Service/CustomerService';
-import Sign_Up from '../pages/Sign_Up/Sign_Up';
-import MyPage from '../pages/MyPage/MyPage';
+import Overseas from "../pages/Overseas/Overseas";
+import Guide from "../pages/Guide/Guide";
+import CustomerService from "../pages/Customer_Service/CustomerService";
+import SignUp from "../pages/SignUp/SignUp";
+import MyPage from "../pages/MyPage/MyPage";
 
 function Router() {
   return (
@@ -24,10 +23,12 @@ function Router() {
         <Route path="/overseas" element={<Overseas />} />
         <Route path="/guide" element={<Guide />} />
         <Route path="/support" element={<CustomerService />} />
-        <Route path="/join" element={<Sign_Up />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/mypage" element={<MyPage />} />
 
-        <Route element={<PrivateRoute />}> {/* 로그인 이후 사용 가능 router */}
+        <Route element={<PrivateRoute />}>
+          {" "}
+          {/* 로그인 이후 사용 가능 router */}
         </Route>
       </Route>
     </Routes>
