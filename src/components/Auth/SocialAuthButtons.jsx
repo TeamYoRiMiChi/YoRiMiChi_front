5a2171e617c9626f6bf9cb2aa09c5f51edd52a93
Gajjamal <87606589+signife@users.noreply.github.com>
2026-09-23 13:42:53 +0900

    feat: connect Google sign-in button to Cognito

--- src/components/Auth/SocialAuthButtons.jsx
+++ src/components/Auth/SocialAuthButtons.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { signInWithRedirect } from 'aws-amplify/auth';
 import GoogleIcon from "../../assets/images/google_social_btn.png";
 import LineIcon from "../../assets/images/line_social_btn.png";
 
 function SocialAuthButtons({ googleText, lineText }) {
+  const [isRedirecting, setIsRedirecting] = useState(false);
+
+  const handleGoogleSignIn = async () => {
+    try {
+      setIsRedirecting(true);
+
+      await signInWithRedirect({
+        provider: 'Google',
+      });
+    } catch (error) {
+      console.error("Failed to start Google sign-in.", error);
+      setIsRedirecting(false);
+    }
+  };
+
   return (
     <>
       <div className="social-divider">
@@ -9,12 +26,12 @@ function SocialAuthButtons({ googleText, lineText }) {
       </div>
 
       <div className="social-signup">
-        <button className="social-button" type="button">
+        <button className="social-button" type="button" onClick={handleGoogleSignIn} disabled={isRedirecting}>
           <img src={GoogleIcon} alt="" />
-          <span>{googleText}</span>
+          <span>{isRedirecting ? 'Googleに移動中...' : googleText}</span>
         </button>
 
-        <button className="social-button" type="button">
+        <button className="social-button" type="button" disabled>
           <img src={LineIcon} alt="" />
           <span>{lineText}</span>
         </button>
