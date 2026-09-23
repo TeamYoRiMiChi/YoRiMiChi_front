51777662d5a86b9b3b490acee2a1e2cbd986ba29
Gajjamal <87606589+signife@users.noreply.github.com>
2026-09-23 13:20:01 +0900

    bug fix : change "throw exception error"

--- src/config/cognito.js
+++ src/config/cognito.js
@@ -8,34 +8,35 @@ const domain = import.meta.env.VITE_COGNITO_DOMAIN;
 
 if (!userPoolId || !userPoolClientId || !domain) {
   throw new Error('Cognito environment variables are missing.');
-}
+} else { 
 
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId,
-      userPoolClientId,
+    Amplify.configure({
+    Auth: {
+        Cognito: {
+        userPoolId,
+        userPoolClientId,
 
-      loginWith: {
-        email: true,
+        loginWith: {
+            email: true,
 
-        oauth: {
-          domain,
-          scopes: ['openid', 'email', 'profile'],
-          redirectSignIn: [
-            `${window.location.origin}/auth/callback`,
-          ],
-          redirectSignOut: [
-            `${window.location.origin}/login`,
-          ],
-          responseType: 'code',
+            oauth: {
+            domain,
+            scopes: ['openid', 'email', 'profile'],
+            redirectSignIn: [
+                `${window.location.origin}/auth/callback`,
+            ],
+            redirectSignOut: [
+                `${window.location.origin}/login`,
+            ],
+            responseType: 'code',
+            },
         },
-      },
+        },
     },
-  },
-});
+    });
 
 
 // Keep tokens only for the current browser tab
 cognitoUserPoolsTokenProvider.setKeyValueStorage(sessionStorage);
+}
